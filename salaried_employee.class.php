9489703f75fd203b8e8b6cc9860ea4b07234d247
AdPatric <44005115+AdPatric@users.noreply.github.com>
2018-10-11 11:37:38 -0400

    Add files via upload

--- salaried_employee.class.php
+++ salaried_employee.class.php
@@ -1,29 +1,38 @@
 <?php
 
 /*
-* Author: Adam Patrick
-* Date: 10/9/18
-* Name: salaried_employee.class.php
-* Description: This class models a salaried employee, subclass of employee
-*/
+ * Author: Adam Patrick
+ * Date: 10/9/18
+ * Name: salaried_employee.class.php
+ * Description: This class models a salaried employee, subclass of employee
+ */
 
 class SalariedEmployee extends Employee {
+
     private $weekly_salary;
+    private static $employee_count;
+
     //define constructor
-    public function __construct($weekly_salary){
+    public function __construct($person, $ssn, $weekly_salary) {
+        parent::__construct($person, $ssn);
         $this->weekly_salary = $weekly_salary;
+        self::$student_count;
     }
+
     //define getters
-    public function getWeeklySalary(){
+    public function getWeeklySalary() {
         return $this->weekly_salary;
     }
-    public function getPaymentAmount(){
+
+    public function getPaymentAmount() {
         return $this->getWeeklySalary();
     }
-    //
-    public function toString(){
+
+    //extend the parent toString method by adding output for weekly salary and payment amount
+    public function toString() {
         parent::toString();
         printf("Weekly Salary: %0.2f", $this->getWeeklySalary());
         printf("Payment Amount: %0.2f", $this->getPaymentAmount());
     }
+
 }
