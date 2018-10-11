008ba0221db2de12ce7212ef761c3f0c435a9b60
AdPatric <44005115+AdPatric@users.noreply.github.com>
2018-10-11 10:51:50 -0400

    Add files via upload

---
+++ commission_employee.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Author Adam Patrick
+ * Date: 10/11/18
+ * Name: commission_employee.class.php
+ * Description: A parent and child class that models a commision employee
+*/
+
+class CommissionEmployee extends Employee {
+    private $sales;
+    private $commission_rate;
+    
+    
+}
